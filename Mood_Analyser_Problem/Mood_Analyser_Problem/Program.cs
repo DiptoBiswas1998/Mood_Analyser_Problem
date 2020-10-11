2679f43cbd71c9ad9e84d00563bf8a25e86d3e8f
unknown <diptobiswas1998@gmail.com>
2020-10-12 01:14:35 +0530

    [Dipto] refactor: Adding Welcome msg

---
+++ Mood_Analyser_Problem/Mood_Analyser_Problem/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Mood_Analyser_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Mood Analyser Problem!");
+        }
+    }
+}
