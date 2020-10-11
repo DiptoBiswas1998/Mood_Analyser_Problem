27d2d71bbab7ea5518b19bc73d5767e4554d5bd4
unknown <diptobiswas1998@gmail.com>
2020-10-12 01:35:45 +0530

    [Dipto] refactor: Responding happy or sad

--- Mood_Analyser_Problem/Mood_Analyser_Problem/Program.cs
+++ Mood_Analyser_Problem/Mood_Analyser_Problem/Program.cs
@@ -6,6 +6,11 @@ namespace Mood_Analyser_Problem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Mood Analyser Problem!");
+            Console.WriteLine("Enter message: ");
+            string message = Console.ReadLine();
+            MoodAnalyser checkmood = new MoodAnalyser(message);
+            string result = checkmood.AnalyseMood(message);
+            Console.WriteLine(result + " MOOD");
         }
     }
 }
